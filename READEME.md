f2c9c69a980975ac944795d5d43a851eba71fafb
zzh-1992 <ezzh1992@icloud.com>
2024-01-08 23:45:29 +0800

    use Springdoc OpenAPI

---
+++ READEME.md
@@ -0,0 +1,12 @@
+
+
+# Springdoc OpenAPI
+
+## UI
+http://localhost:8080/swagger-ui.html
+
+## JSON
+http://localhost:8080/v3/api-docs
+
+![img.png](img.png)
+
--- src/main/java/com/grapefruit/api/YourController.java
+++ src/main/java/com/grapefruit/api/YourController.java
@@ -1,7 +1,5 @@
 package com.grapefruit.api;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@Api(tags = "Your API Controller")
 public class YourController {
 
     @GetMapping("/hello")
-    @ApiOperation("Get a hello message")
     public String getHello(@RequestHeader(value = "cookie") String cookie) {
 
         return "Hello, Swagger!" + cookie;
